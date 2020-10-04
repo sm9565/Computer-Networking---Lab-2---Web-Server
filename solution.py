c98018ede9d9ae3aebc77e9c2b4ddea14676fc12
sm9565 <70976372+sm9565@users.noreply.github.com>
2020-10-03 20:51:33 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -21,7 +21,7 @@ def webServer(port=13331):
         connectionSocket, addr = serverSocket.accept()
 
         try:
-            message = connectionSocket.recv(1024)
+            message = connectionSocket.recv(2048)
             filename = message.split()[1]
             f = open(filename[1:])
             outputdata = f.read()
